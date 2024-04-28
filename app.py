dcfdbeaa91f6865f4983a32fb1661df41bea9604
rtwork321 <fen.iks@inbox.lv>
2024-04-28 19:01:31 +0300

    b

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+import sys
 from flask import Flask, jsonify
-import argparse
 
 app = Flask(__name__)
 
@@ -8,10 +8,4 @@ def greetings():
     return jsonify({"greeting": "Hello from Python App!"})
 
 if __name__ == '__main__':
-    # Parse command-line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--port', type=int, required=True, help='Port number for the Flask app')
-    args = parser.parse_args()
-
-    # Run the Flask app on the specified port
-    app.run(host="0.0.0.0", debug=True, port=args.port)
+    app.run(host="0.0.0.0", debug=True, port="3000")
