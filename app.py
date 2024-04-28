fc1ee7cca96a2f253a9f0e20cdc8fbe4cb29f4a5
rtwork321 <fen.iks@inbox.lv>
2024-04-28 17:15:19 +0300

    n

--- app.py
+++ app.py
@@ -7,4 +7,11 @@ def greetings():
     return jsonify({"greeting": "Hello from Python App!"})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port="3000")
+
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    else:
+        # Default port
+        port = 3000
+
+    app.run(host="0.0.0.0", debug=True, port=port)
