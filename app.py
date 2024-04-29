fcc147ce7a9ce8b1a85b12d095731a2caa5f2b31
rtwork321 <fen.iks@inbox.lv>
2024-04-29 19:00:11 +0300

    n

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import sys
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -8,6 +8,4 @@ def greetings():
     return jsonify({"greeting": "Hello from Python App!"})
 
 if __name__ == '__main__':
-    # Use the port specified in the environment variable or default to 3000
-    port = int(os.getenv('PORT'))
-    app.run(host="0.0.0.0", debug=True, port=port)
+    app.run(host="0.0.0.0", debug=True, port="3000")
