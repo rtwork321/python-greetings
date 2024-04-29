63352af7142b56fb07fae461560f9fa131bdbc27
rtwork321 <fen.iks@inbox.lv>
2024-04-29 17:44:16 +0300

    n

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def greetings():
 
 if __name__ == '__main__':
     # Use the port specified in the environment variable or default to 3000
-    port = int(os.getenv('PORT', 3000))
+    port = int(os.getenv('PORT'))
     app.run(host="0.0.0.0", debug=True, port=port)
