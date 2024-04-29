973f40e48990eebf64e1e76a82d5319fb824c17a
rtwork321 <fen.iks@inbox.lv>
2024-04-29 19:14:11 +0300

    b

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ def greetings():
     return jsonify({"greeting": "Hello from Python App!"})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port="3000")
+    # app.run(host="0.0.0.0", debug=True, port="3000")
+    app.run(host="0.0.0.0", debug=True)
